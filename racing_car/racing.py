13740e87bdf1995d8f51b3565d0034e2f8841d65
Yan Ren <ryan.ren3@gmail.com>
2019-09-28 05:13:39 -0400

    Display Images

--- racing_car/racing.py
+++ racing_car/racing.py
@@ -1,12 +1,23 @@
 import pygame, sys
 
-pygame.init()
+# constant
+FPS = 60
+WHITE = (255, 255, 255)
+# variables
+window_width = 800
+window_height = 600
+crashed = False
 
-display = pygame.display.set_mode((800, 600))
+pygame.init()
+display = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('Race Car')
 clock = pygame.time.Clock()
-crashed = False
-FPS = 60
+car = pygame.image.load('./img/car1.png').convert_alpha()
+
+
+def place_car(x, y):
+    display.blit(car, (x, y))
+
 
 while not crashed:
 
@@ -14,8 +25,10 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
 
-        print(event)
+        # print(event)
 
+    display.fill(WHITE)
+    place_car(window_width * 0.45, window_height * 0.8)
     pygame.display.update()
     clock.tick(FPS)
 
