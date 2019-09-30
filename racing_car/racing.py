1be232f0afbd6e1caaf3f6a3e25e28a90c6f1430
yan-ren <ryan.ren3@gmail.com>
2019-09-30 15:55:36 -0400

    Moving an Image

--- racing_car/racing.py
+++ racing_car/racing.py
@@ -7,6 +7,10 @@ WHITE = (255, 255, 255)
 window_width = 800
 window_height = 600
 crashed = False
+x = window_width * 0.45
+y = window_height * 0.8
+x_change = 0
+y_change = 9
 
 pygame.init()
 display = pygame.display.set_mode((window_width, window_height))
@@ -24,11 +28,21 @@ while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
 
-        # print(event)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                x_change = 0
+
+    x += x_change
+    # print(event)
 
     display.fill(WHITE)
-    place_car(window_width * 0.45, window_height * 0.8)
+    place_car(x, y)
     pygame.display.update()
     clock.tick(FPS)
 
